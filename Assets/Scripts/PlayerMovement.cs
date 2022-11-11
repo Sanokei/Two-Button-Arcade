4582d171387e26ecb633d907967d66675a953a7b
Sano <36809574+Sanokei@users.noreply.github.com>
2022-11-10 19:52:37 -0500

    Initial commit

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,89 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkingSpeed = 7.5f;
+    [SerializeField] private float runningSpeed = 11.5f;
+    [SerializeField] private float jumpSpeed = 8.0f;
+    [SerializeField] private float gravity = 20.0f;
+    [SerializeField] private float lookSpeed = 2.0f;
+    [SerializeField] private float lookXLimit = 45.0f;
+    [SerializeField] private CharacterController characterController;
+    public Camera playerCamera;
+    
+    // private
+    Vector3 moveDirection = Vector3.zero;
+    float rotationX = 0;
+    [HideInInspector] public bool canMove = true;
+
+    void Start()
+    {
+        characterController = characterController ?? GetComponent<CharacterController>();
+
+        // Needs to be unlocked to work with World Space UI
+        Cursor.lockState = CursorLockMode.Locked;
+        // Kinda screws with the World Space UI
+        // heavily recommend enabling visiblity when in UI mode
+        // Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        // We are grounded, so recalculate move direction based on axes
+        // dont know if this is efficient
+        Vector3 forward = transform.TransformDirection(Vector3.forward);
+        Vector3 right = transform.TransformDirection(Vector3.right);
+        
+        // Press Left Shift to run
+        bool isRunning = false;
+        /*
+        After some play testing I realized running was not
+        just useless, people would hold it down instead of 
+        using both walking and running since there was no
+        game mechanical difference.
+        so until i make some sort of foot step sound mechanic
+        i will be keeping this commented out.
+        */
+        // isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        // Calculate how fast we should be moving
+        float curSpeedX = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
+        float curSpeedY = canMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
+        float movementDirectionY = moveDirection.y;
+        moveDirection = (forward * curSpeedX) + (right * curSpeedY);
+
+        // Jump
+        if (Input.GetButton("Jump") && canMove && characterController.isGrounded)
+        {
+            moveDirection.y = jumpSpeed;
+        }
+        else
+        {
+            moveDirection.y = movementDirectionY;
+        }
+
+        // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
+        // when the moveDirection is multiplied by deltaTime). This is because gravity should be applied
+        // as an acceleration (ms^-2)
+        if (!characterController.isGrounded)
+        {
+            moveDirection.y -= gravity * Time.deltaTime;
+        }
+
+        // Move the controller
+        characterController.Move(moveDirection * Time.deltaTime);
+
+        // Player and Camera rotation
+        if (canMove)
+        { 
+            // TODO: This is pretty gross but it works for now
+            rotationX += -Input.GetAxis("Mouse Y") * lookSpeed;
+            rotationX = Mathf.Clamp(rotationX, -lookXLimit, lookXLimit);
+            playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
+            transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
+        }
+    }
+}
