e73c700c0db49c3fce27b333624dcc5c0345b464
Sano <36809574+Sanokei@users.noreply.github.com>
2022-11-14 11:37:15 -0500

    Added Totality

---
+++ Assets/HierarchyElement.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HierarchyElement : MonoBehaviour
+{
+    [SerializeField] TMP_InputField _input;
+    [SerializeField] TMP_TextElement _text;
+    
+     
+}
